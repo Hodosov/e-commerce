1091675f1643aacdf3373a81cc6aa219645dedc5
Hodosov Yaroslav <hodosov.y@mail.ru>
2021-12-01 21:44:21 +0300

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:HomePage()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
 }
 
 class HomePage extends StatefulWidget {
@@ -21,9 +19,38 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.red,
         title: const Text("FashApp"),
         actions: <Widget>[
-          IconButton(onPressed: (){}, icon: const Icon(Icons.search, color: Colors.white,)),
-           IconButton(onPressed: (){}, icon: const Icon(Icons.shopping_cart, color: Colors.white,))
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ))
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: const Text('Santos'),
+              accountEmail: const Text("Santos@gmail.com"),
+              currentAccountPicture: GestureDetector(
+                child: const CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
