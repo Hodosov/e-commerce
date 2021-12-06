fd277a173ada16d4d821097ff7abc742314c00fd
Hodosov Yaroslav <hodosov.y@mail.ru>
2021-12-06 21:42:50 +0300

    add actions buttons

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -23,7 +23,7 @@ class _ProductDetailsState extends State<ProductDetails> {
       appBar: AppBar(
         elevation: 0.0,
         backgroundColor: Colors.red,
-        title:  Text(widget.product_detail_name),
+        title: Text(widget.product_detail_name),
         actions: <Widget>[
           IconButton(
               onPressed: () {},
@@ -46,9 +46,92 @@ class _ProductDetailsState extends State<ProductDetails> {
             child: GridTile(
               child: Container(
                 color: Colors.white,
-              child: Image.asset(widget.product_detail_picture),
+                child: Image.asset(widget.product_detail_picture),
+              ),
+              footer: Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: Text(
+                    widget.product_detail_name,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  title: Row(
+                    children: [
+                      Expanded(
+                          child: Text(
+                        "\$${widget.product_detail_old_price}",
+                        style: const TextStyle(
+                            color: Colors.grey,
+                            decoration: TextDecoration.lineThrough),
+                      )),
+                      Expanded(
+                          child: Text(
+                        "\$${widget.product_detail_new_price}",
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.red),
+                      ))
+                    ],
+                  ),
+                ),
               ),
             ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                  child: MaterialButton(
+                onPressed: () {},
+                color: Colors.white,
+                textColor: Colors.grey,
+                elevation: 0.2,
+                child: Row(
+                  children: const [
+                    Expanded(child: Text("Size")),
+                    Expanded(child: Icon(Icons.arrow_drop_down))
+                  ],
+                ),
+              )),
+              Expanded(
+                  child: MaterialButton(
+                onPressed: () {},
+                color: Colors.white,
+                textColor: Colors.grey,
+                elevation: 0.2,
+                child: Row(
+                  children: const [
+                    Expanded(child: Text("Color")),
+                    Expanded(child: Icon(Icons.arrow_drop_down))
+                  ],
+                ),
+              )),
+              Expanded(
+                  child: MaterialButton(
+                onPressed: () {},
+                color: Colors.white,
+                textColor: Colors.grey,
+                elevation: 0.2,
+                child: Row(
+                  children: const [
+                    Expanded(child: Text("Qty")),
+                    Expanded(child: Icon(Icons.arrow_drop_down))
+                  ],
+                ),
+              )),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                  child: MaterialButton(
+                      onPressed: () {},
+                      color: Colors.red,
+                      textColor: Colors.white,
+                      elevation: 0.2,
+                      child: const Text('Buy now'))),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart, color: Colors.red,)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_border, color: Colors.red)),
+            ],
           )
         ],
       ),
